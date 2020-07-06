5452d06956eac266b9fbe3ac072d9c6ea3fe1631
ngovanhai <anhhai19xx@gmail.com>
2020-07-06 21:35:09 +0700

    home

---
+++ media/image/Cổng thông tin tuyển sinh_files/FaqViewService.js
@@ -0,0 +1,62 @@
+﻿app.service("faqService", function ($http) {
+    'use strict';
+    
+    // Get All Contents
+    this.getFaqContent = function (PortalId, ModuleId, CateId, Status, txtKeyword, PageIndex, PageSize) {
+        var response = $http({
+            method: "get",
+            url: baseUrl + "/faq/listcontent",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            params: {
+                portalid: PortalId,
+                moduleid: ModuleId,
+                cateid: CateId,
+                status: Status,
+                txtKeyword: txtKeyword,
+                pageindex: PageIndex,
+                pagesize: PageSize
+            }
+        });
+        return response;
+    }
+
+    // get By Id
+    this.getItem = function (Id) {
+        var response = $http({
+            method: "get",
+            url: baseUrl + "/faq/getbyid",
+            params: {
+                id: JSON.stringify(Id)
+            }
+        });
+        return response;
+    }
+
+    // Get All Categories
+    this.getFaqCategory = function (PortalId, ModuleId, Status, txtKeyword, PageIndex, PageSize) {
+        var response = $http({
+            method: "get",
+            url: baseUrl + "/faq/listcategory",
+            params: {
+                PortalId: PortalId,
+                ModuleId: ModuleId,
+                Status: Status,
+                txtKeyword: txtKeyword,
+                pageindex: PageIndex,
+                pagesize: PageSize
+            }
+        });
+        return response;
+    }
+    // Update Content 
+    this.UpdateItem = function (obj) {
+        var response = $http({
+            method: "post",
+            url: baseUrl + "/faq/save",
+            data: JSON.stringify(obj),
+            dataType: "json"
+        });
+        return response;
+    }//end
+});
